58de8cec0aa4999c5838da842614df709f06ac37
Igor Osadchyi <igorosa.com@gmail.com>
2020-11-18 19:47:31 +0200

    feat: image on items

--- gem-puzzle/src/js/cell.js
+++ gem-puzzle/src/js/cell.js
@@ -1,9 +1,10 @@
 export default function Cell({
-  top, left, height, width, inner,
+  top, left, height, width, inner, imageSrc,
 }) {
   this.top = top;
   this.left = left;
   this.inner = inner;
+  this.isEmpty = false;
 
   const item = document.createElement('div');
   item.style.width = `${width}%`;
@@ -12,8 +13,14 @@ export default function Cell({
   item.style.left = `${this.left * width}%`;
   item.classList.add('cell');
   item.innerHTML = inner || '';
-  item.dataset.top = top;
-  item.dataset.left = left;
+
+  // background image
+  item.style.backgroundSize = '400% 400%';
+  item.style.backgroundImage = `url(${imageSrc})`;
+  item.style.backgroundPosition = `-${((inner - 1) % 4) * 100}% -${
+    Math.floor((inner - 1) / 4) * 100
+  }%`;
 
   this.get = () => item;
+  this.node = this.get();
 }
--- gem-puzzle/src/js/field.js
+++ gem-puzzle/src/js/field.js
@@ -7,6 +7,8 @@ import {
 import { createDOMElement, timeFormatter } from './helpers';
 import popup from './popup';
 
+import imageSrc from '../images/125.jpg';
+
 export default function Field() {
   this.storage = new Storage();
   this.cells = [];
@@ -86,17 +88,22 @@ export default function Field() {
     // resize field on resize window
     window.addEventListener('resize', setGameAreaSize);
 
-    app.append(header, gameArea);
+    const tmp = document.createElement('div');
+    tmp.style.fontSize = '1.2rem';
+    tmp.innerText = '* Цифры оставлены для удобства проверки!';
+
+    app.append(header, gameArea, tmp);
 
     // empty cell
-    const emptyCell = new Cell({
+    const empty = new Cell({
       height: itemSize,
       width: itemSize,
       ...EMPTY_ITEM,
     });
-    const emptyNode = emptyCell.get();
-    emptyNode.classList.add('cell-empty');
-    gameArea.appendChild(emptyNode);
+    empty.isEmpty = true;
+    this.cells.push(empty);
+    empty.node.classList.add('cell-empty');
+    gameArea.appendChild(empty.node);
 
     // cells
     for (let i = 0; i < TOTAL_ITEMS; i += 1) {
@@ -109,31 +116,34 @@ export default function Field() {
         top,
         left,
         inner: randomNumbers[i] + 1,
+        imageSrc,
       });
-      const itemNode = item.get();
 
       this.cells.push(item);
 
-      gameArea.appendChild(itemNode);
+      gameArea.appendChild(item.node);
 
-      itemNode.addEventListener('click', () => {
-        const leftDiff = Math.abs(item.left - emptyCell.left);
-        const topDiff = Math.abs(item.top - emptyCell.top);
+      item.node.addEventListener('click', () => {
+        const leftDiff = Math.abs(item.left - empty.left);
+        const topDiff = Math.abs(item.top - empty.top);
         if (leftDiff + topDiff === 1) {
           // swap cell positions
-          [item.top, emptyCell.top] = [emptyCell.top, item.top];
-          [item.left, emptyCell.left] = [emptyCell.left, item.left];
-          [itemNode.style.top, emptyNode.style.top] = [emptyNode.style.top, itemNode.style.top];
-          [itemNode.style.left, emptyNode.style.left] = [emptyNode.style.left, itemNode.style.left];
+          [item.top, empty.top] = [empty.top, item.top];
+          [item.left, empty.left] = [empty.left, item.left];
+          [item.node.style.top, empty.node.style.top] = [empty.node.style.top, item.node.style.top];
+          [item.node.style.left, empty.node.style.left] = [
+            empty.node.style.left,
+            item.node.style.left,
+          ];
 
           // update moves
           this.storage.incrementMoves();
           counterData.innerText = this.storage.getProp('moves');
 
           // check if current position win
-          this.isFinished = this.cells.every(
-            (cell) => cell.inner === cell.top * ITEMS_IN_A_ROW + cell.left + 1,
-          );
+          this.isFinished = this.cells
+            .filter((x) => !x.isEmpty)
+            .every((cell) => cell.inner === cell.top * ITEMS_IN_A_ROW + cell.left + 1);
 
           if (this.isFinished) {
             if (this.timerId) {
