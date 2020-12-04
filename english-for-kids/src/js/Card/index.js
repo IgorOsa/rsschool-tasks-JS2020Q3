b28aebf3183064305a03750bb1d25082f5a0e2e9
Igor Osadchyi <igorosa.com@gmail.com>
2020-12-04 19:20:35 +0200

    feat: card partial implementation

--- english-for-kids/src/js/Card/index.js
+++ english-for-kids/src/js/Card/index.js
@@ -1,9 +1,9 @@
-export default function Card(props) {
-  // eslint-disable-next-line no-unused-vars
-  const { title, word, translation, image, audioSrc } = props;
+import './index.scss';
 
+// eslint-disable-next-line no-unused-vars
+function createCard({ title, word, translation, image, audioSrc }) {
   const card = document.createElement('div');
-  card.className = 'card mb-4';
+  card.className = 'card mb-4 shadow';
 
   const cardBody = document.createElement('div');
   cardBody.className = 'card-body';
@@ -16,16 +16,43 @@ export default function Card(props) {
     cardBody.appendChild(cardImage);
   }
 
-  if (title || word) {
+  if (title || word || translation) {
     const cardTitle = document.createElement('h5');
     cardTitle.className = 'card-title text-center';
-    cardTitle.innerText = title || word;
+    cardTitle.innerText = title || word || translation;
     cardBody.appendChild(cardTitle);
   }
 
-  // const button = `<a href="#" class="btn btn-primary">Go somewhere</a>`;
+  if (word) {
+    const button = document.createElement('a');
+    button.innerHTML = '<a href="#" class="text-center">R</a>';
+    cardBody.appendChild(button);
+    button.addEventListener('click', () => {
+      // todo: implement function for rotation card to see an answer
+    });
+  }
 
-  card.appendChild(cardBody);
+  if (audioSrc) {
+    const audio = document.createElement('audio');
+    audio.src = audioSrc;
+    cardBody.addEventListener('click', () => {
+      audio.play();
+    });
+    cardBody.appendChild(audio);
+  }
+
+  card.append(cardBody);
 
   return card;
+}
+
+export default function Card(props) {
+  // eslint-disable-next-line no-unused-vars
+  const { title, word, translation, image, audioSrc, back } = props;
+
+  const cardFront = createCard({
+    title, word, image, audioSrc,
+  });
+
+  return cardFront;
 }
