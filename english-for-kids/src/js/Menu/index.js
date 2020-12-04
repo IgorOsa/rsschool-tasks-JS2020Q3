cbcf796c2db89836894268fb0a661e4a5ee4a7e1
Igor Osadchyi <igorosa.com@gmail.com>
2020-12-04 19:21:32 +0200

    feat: side menu items

--- english-for-kids/src/js/Menu/index.js
+++ english-for-kids/src/js/Menu/index.js
@@ -1,8 +1,9 @@
-// eslint-disable-next-line no-unused-vars
+import { renderMain, renderCategory } from '../Main';
 import { toggleNav } from '../tools';
 import './index.scss';
 
 export default function Menu(props) {
+  const storage = props;
   const [categories] = props.data;
 
   const aside = document.createElement('aside');
@@ -15,14 +16,45 @@ export default function Menu(props) {
   aside.appendChild(closeBtn);
   closeBtn.addEventListener('click', toggleNav);
 
+  const mainPageLink = document.createElement('a');
+  mainPageLink.href = '#';
+  mainPageLink.innerHTML = 'Main page';
+  mainPageLink.addEventListener('click', () => {
+    const main = document.getElementById('main');
+    renderMain({
+      ...storage, main,
+    });
+    toggleNav();
+  });
+  aside.appendChild(mainPageLink);
+
   if (categories) {
-    categories.forEach((el) => {
+    categories.forEach((el, i) => {
       const menuItem = document.createElement('a');
       menuItem.href = '#';
       menuItem.innerHTML = el.title;
+      menuItem.addEventListener('click', () => {
+        const node = document.getElementById('cards-container');
+        renderCategory({
+          words: props.data[i + 1], node,
+        });
+        toggleNav();
+      });
       aside.appendChild(menuItem);
     });
   }
+
+  // todo implement function to clome menu after click outside
+  // eslint-disable-next-line no-unused-vars
+  const checkOutsideClick = (event) => {
+    const isClickInside = aside.contains(event.target);
+    if (!isClickInside) {
+      toggleNav();
+      // document.removeEventListener('click', checkOutsideClick);
+    }
+  };
+
+  // document.addEventListener('click', checkOutsideClick);
 
   return aside;
 }
